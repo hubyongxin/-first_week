8a4f8b6f0cc28ad97ae6a58165bea7b6b0918343
wuyongxin <1491922219@qq.com>
2021-11-18 18:27:40 +0800

    完成添加删除等

--- demo/src/views/ProjectApplication.vue
+++ demo/src/views/ProjectApplication.vue
@@ -26,8 +26,8 @@
             <el-button type="primary" icon="el-icon-search" size="small"
               >查询</el-button
             >
-            <el-button type="primary" size="small" @click="goAdd"
-              ><i class="el-icon-upload el-icon--left"></i>添加</el-button
+            <el-button type="success" size="small" @click="goAdd"
+              ><i class="el-icon-plus el-icon--left"></i>添加</el-button
             >
           </span>
         </el-form>
@@ -35,7 +35,18 @@
       <!-- 信息列表区域 -->
       <div class="list">
         <el-table :data="tableData" style="width: 100%" height="780">
-          <el-table-column fixed prop="name" label="项目信息" width="204">
+          <el-table-column
+            fixed
+            prop="name ,proguid"
+            label="项目信息"
+            width="204"
+          >
+            <template slot-scope="scope">
+              <div>
+                <el-button type="text">{{ scope.row.name }}</el-button>
+              </div>
+              <div>{{ scope.row.proguid }}</div>
+            </template>
           </el-table-column>
           <el-table-column prop="amount" label="送审金额（万元）" width="135">
           </el-table-column>
@@ -43,30 +54,93 @@
           </el-table-column>
           <el-table-column prop="construct_dwname" label="建设单位" width="162">
           </el-table-column>
-          <el-table-column prop="pro_lxr" label="联系人信息" width="162">
+          <el-table-column
+            prop="pro_lxr ,pro_tel"
+            label="联系人信息"
+            width="162"
+          >
+            <template slot-scope="scope">
+              <div>{{ scope.row.pro_lxr }}</div>
+              <div>{{ scope.row.pro_tel }}</div>
+            </template>
           </el-table-column>
           <el-table-column
-            prop="price_starttime"
+            prop="price_starttime , price_endtime"
             label="报价时间范围"
             width="244"
           >
+            <template slot-scope="scope">
+              <div>{{ scope.row.price_starttime }}</div>
+              <div>{{ scope.row.price_endtime }}</div>
+            </template>
           </el-table-column>
           <el-table-column prop="shbj" label="审核状态" width="119">
+            <template slot-scope="scope">
+   
+              <el-tag type="primary" v-if="scope.row.shbj == '审核中'">{{
+                scope.row.shbj
+              }}</el-tag>
+              <el-tag type="success" v-if="scope.row.shbj == '通过'">{{
+                scope.row.shbj
+              }}</el-tag>
+              <el-tag type="info" v-if="scope.row.shbj == '仅暂存'">{{
+                scope.row.shbj
+              }}</el-tag>
+            </template>
           </el-table-column>
-          <el-table-column prop="isend" label="评审状态" width="119">
+          <el-table-column label="评审状态" prop="isend" width="119">
+            <template slot-scope="scope">
+              <el-tag type="warning" v-if="scope.row.isend == '未评定'">{{
+                scope.row.isend
+              }}</el-tag>
+              <el-tag type="success" v-if="scope.row.isend == '已评定'">{{
+                scope.row.isend
+              }}</el-tag>
+            </template>
           </el-table-column>
+
           <el-table-column fixed="right" label="操作">
             <template slot-scope="scope">
-              <el-button @click.native.prevent="deleteRow(scope.$index, tableData)" type="text" size="small">详情</el-button>
+              <el-button
+                @click.native.prevent="deleteRow(scope.$index, tableData)"
+                type="text"
+                size="small"
+                >详情</el-button
+              >
+              <el-button
+                @click.native.prevent="deleteRow(scope.$index, tableData)"
+                type="text"
+                size="small"
+                >编辑</el-button
+              >
+              <el-button
+                type="text"
+                size="small"
+                @click="removeProjectById(scope.row.proguid)"
+                >删除</el-button
+              >
+              <el-button
+                @click.native.prevent="deleteRow(scope.$index, tableData)"
+                type="text"
+                size="small"
+                v-if="scope.row.isend == '待审核'"
+                >查看结果</el-button
+              >
             </template>
           </el-table-column>
         </el-table>
       </div>
       <!-- 分页区域 -->
       <div class="page">
-        <el-pagination background layout=" total,prev, pager, next" :total="this.listTotal" :page-size='this.pageSize' :current-page="this.currentPage"
-          @current-change="handleCurrentChange">
-  </el-pagination>
+        <el-pagination
+          background
+          layout=" total,prev, pager, next"
+          :total="this.listTotal"
+          :page-size="this.pageSize"
+          :current-page="this.currentPage"
+          @current-change="handleCurrentChange"
+        >
+        </el-pagination>
       </div>
     </div>
   </div>
@@ -74,6 +148,7 @@
 
 <script>
 import request from "../utlis/request";
+import qs from 'qs'
 export default {
   data() {
     return {
@@ -93,19 +168,18 @@ export default {
       //列表数据
       tableData: [],
       //分页总条数
-      listTotal:0,
+      listTotal: 0,
       //每页显示数据
-      pageSize:20,
+      pageSize: 20,
       //显示当前页
-      currentPage:1
-
+      currentPage: 1,
     };
   },
   methods: {
     //页数变化
-    handleCurrentChange(currentPage){
-        this.currentPage = currentPage
-        this.getList()
+    handleCurrentChange(currentPage) {
+      this.currentPage = currentPage;
+      this.getList();
     },
 
     //获取列表
@@ -115,50 +189,78 @@ export default {
         {
           params: {
             pageindex: this.currentPage,
-            // sortname: addtime,
-            // sortorder: desc,
+            sortname: "addtime",
+            sortorder: "desc",
             pagesize: 20,
-            // business: financial-review
+            business: "financial-review",
           },
         }
       );
-      	res.data.rows.map(function (val) {
-              if (val.shbj == 0) {
-                val.shbj = '仅暂存'
-              } else if (val.shbj == 1) {
-                val.shbj = '待审核'
-              }
-               else {
-                val.shbj = '审核中'
-              }
-            })
-      		res.data.rows.map(function (val) {
-              if (val.isend == 0) {
-                val.isend = '未审核'
-              } 
-               else {
-                val.isend = '已评定'
-              }
-            })
+      res.data.rows.map(function (val) {
+        if (val.shbj == 0) {
+          val.shbj = "仅暂存";
+        } else if (val.shbj == 1) {
+          val.shbj = "待审核";
+        } else if (val.shbj == 2) {
+          val.shbj = "退回";
+        } else if (val.shbj == 3) {
+          val.shbj = "审核中";
+        } else if (val.shbj == 9) {
+          val.shbj = "通过";
+        }
+      });
+      res.data.rows.map(function (val) {
+        if (val.isend == 1) {
+          val.isend = "已评定";
+        } else if (val.isend == 0) {
+          val.isend = "未评定";
+        }
+      });
 
-      // console.log(res);
+      console.log(res);
       this.tableData = res.data.rows;
-      this.listTotal = res.data.total
+      this.listTotal = res.data.total;
+    },
+    async removeProjectById(proguid) {
+      try {
+        //弹框询问是否删除
+        const confirmResult = await this.$confirm(
+          "删除后无法撤销，确认删除操作？","提示",
+          {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning",
+          }
+        ).catch((err) => {
+          return err;
+        });
+        //用户确认删除，则返回值为字符串confirm
+        //用户取消删除，则返回值为字符串cancel
+        // console.log(confirmResult);
+        if (confirmResult === "cancel") {
+          return this.$message.info("取消删除");
+        } 
+        let params = {proguid}
+        const res = await request.post("/demandresponse/demander/demand/del.json",qs.stringify(params));
+        if (res.data.code == "v") {
+          this.getList();
+          return this.$message.success("删除成功！")
+        }else{
+          this.$message.error("删除失败！");
+        }
+      } catch (error) {
+        console.log(error, "系统错误");
+      }
+    
     },
 
-    
     //添加页面
     goAdd() {
       this.$router.push("/projectadd");
     },
-
-
   },
   created() {
-   
     this.getList();
-
-    
   },
 };
 </script>
@@ -184,9 +286,12 @@ export default {
     margin: 10px 10px 10px 0;
   }
 }
-.page{
- .el-pagination{
-   float:right
- }
+.page {
+  .el-pagination {
+    float: right;
+  }
+}
+.bs {
+  display: inline;
 }
 </style>
--- demo/src/views/ProjectAudit.vue
+++ demo/src/views/ProjectAudit.vue
@@ -1,53 +1,179 @@
 <template>
   <div>
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" size="small">
-      <el-form-item label="活动名称" prop="name">
-        <el-input v-model="ruleForm.name"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
+  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px" class="demo-ruleForm">
+  <el-form-item label="活动名称" prop="name">
+     <el-input v-model="ruleForm.name" size="small" ></el-input>
+  </el-form-item>
+
+  <el-form-item label="送审类别" prop="type">
+    <el-radio-group v-model="ruleForm.type">
+      <el-radio label="概算"></el-radio>
+      <el-radio label="预算"></el-radio>
+      <el-radio label="标底"></el-radio>
+      <el-radio label="结算"></el-radio>
+      <el-radio label="决算"></el-radio>
+      <el-radio label="其他"></el-radio>
+    </el-radio-group>
+  </el-form-item>
+
+  <el-form-item label="送审金额" prop="money">
+     <el-input-number v-model="ruleForm.money" :min="0" size="small"></el-input-number> <span>（万元）</span>
+  </el-form-item>
+
+  <el-form-item label="评审年度" prop="year">
+      <el-date-picker v-model="ruleForm.year" type="year" placeholder="选择年" size="small" value-format="yyyy"></el-date-picker>
+  </el-form-item>
+
+  <el-form-item label="所属区域" prop="area">
+      <!-- <el-select v-model="ruleForm.area" placeholder="请选择区域" size="small">
+        <el-option :label="item.cityname" :value="item.citycode" v-for="item in cityList " :key ="item.citycode"></el-option>
+      
+    </el-select> -->
+  <el-cascader v-model="this.cityList.citycode" :options="cityList"></el-cascader>
+
+
+  </el-form-item>
+
+    <el-form-item label="建设单位" prop="construction">
+      <el-input v-model="ruleForm.construction" size="small"> </el-input>
+  </el-form-item>
+
+    <el-form-item label="建设单位地址" prop="constructionArea">
+      <el-input v-model="ruleForm.constructionArea" size="small"> </el-input>
+  </el-form-item>
+
+    <el-form-item label="项目联系人" prop="person">
+     <el-input v-model="ruleForm.person" size="small"> </el-input>
+  </el-form-item>
+
+    <el-form-item label="联系人手机号码" prop="personTel">
+    <el-input v-model="ruleForm.personTel" size="small"> </el-input>
+  </el-form-item>
+
+    <el-form-item label="报价开始时间" prop="start">
+      <el-date-picker v-model="ruleForm.start" type="datetime" placeholder="选择日期时间" size="small" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
+  </el-form-item>
+
+    <el-form-item label="报价结束时间" prop="end">
+      <el-date-picker v-model="ruleForm.end" type="datetime" placeholder="选择日期时间" size="small" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
+  </el-form-item>
+
+    <el-form-item label="报价需提交资料说明" prop="desc">
+    <el-input type="textarea" v-model="ruleForm.desc" size="small"></el-input>
+  </el-form-item>
+
+  <el-form-item>
+    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+    <el-button @click="resetForm('ruleForm')">重置</el-button>
+  </el-form-item>
+</el-form>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      ruleForm: {
-        name: '',
+import request from "../utlis/request";
+import qs from 'qs'
+  export default {
+    data() {
+      return {
+        ruleForm: {
+          name:"",
+          type:"",
+          money:"",
+          year:"",
+          area:'',
+          construction:"",
+          constructionArea:"",
+          person:"",
+          personTel:"",
+          start:"",
+          end:"",
+          desc:"",
+          
       },
-      rules: {
-        name: [
-          { required: true, message: '请输入活动名称', trigger: 'blur' },
-          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' },
-        ],
-      },
-    }
-  },
-  methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          console.log('error submit!!')
-          return false
+       cityname:[],
+      //城市列表数据
+       cityList:[],
+      
+        rules: {
+          name: [
+            { required: true, message: '请输入活动名称', trigger: 'blur' },
+          ],
+          desc: [
+            { required: true, message: '报价需提交资料说明', trigger: 'blur' }
+          ]
         }
-      })
+      };
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
+    methods: {
+     async submitForm() {
+
+      try {
+        let params = {
+        rowguid :'',
+        proguid :'',
+        type : '01',
+        typename :'概算', 
+        amount : this.ruleForm.money,
+        review_year : this.ruleForm.year,
+        citycode : 130101,
+        cityname : '石家庄市-市辖区',
+        shdwcode : 'E0000011323',
+        shdwname : '中心1',
+        construct_dwname: this.ruleForm.construction,
+        construct_address : this.ruleForm.constructionArea,
+        pro_lxr : this.ruleForm.person,
+        pro_tel : this.ruleForm.personTel,
+        price_starttime : this.ruleForm.start,
+        price_endtime : this.ruleForm.end,
+        reason : this.ruleForm.desc,
+        isend : '',
+        isinvoice :'',
+        name : this.ruleForm.name,
+        shbj :'1',
+        adduserid :'',
+        addusername :'',
+        adddwcode :'',
+        adddwname :'',
+        shyj :'',
+        scbj :'',
+        addtime :'',
+        sfzhongbiao:'',
+        isptsyf:'',
+        fileguid:'5b296090-1d4f-4fc1-8db4-40a62be8438e'
+
+       }
+       const res = await request.post("/demandresponse/demander/demand/edit.json",qs.stringify(params))
+       console.log(res);
+      } catch (error) {
+        console.log(error,'系统错误');
+      }
+
+
+      
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+
+      //获取城市列表
+     async getCityList(){
+        try {
+           const res = await request.get("/demandresponse/sys/general/citylist.json")
+           this.cityList = res.data.tdata
+           console.log(this.cityList);
+        } catch (error) {
+          console.log(error, '系统接口错误')
+        }
+        
+      }
     },
-  },
-}
+    created() {
+      this.getCityList()
+    },
+  }
 </script>
 
-<style lang="less" scoped>
-.el-form {
-  margin: 10px 0;
-  border: grey 1px solid;
-}
+<style>
+
 </style>
